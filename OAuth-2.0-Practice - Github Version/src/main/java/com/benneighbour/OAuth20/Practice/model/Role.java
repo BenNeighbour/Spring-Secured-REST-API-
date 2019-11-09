4dad74116ce6cf65cb05b699076061360535bc8a
Ben Neighbour <45554944+BenNeighbour@users.noreply.github.com>
2019-11-09 20:42:04 +0000

    Adding comments
    For making the code easier to read for other developers

--- OAuth-2.0-Practice - Github Version/src/main/java/com/benneighbour/OAuth20/Practice/model/Role.java
+++ OAuth-2.0-Practice - Github Version/src/main/java/com/benneighbour/OAuth20/Practice/model/Role.java
@@ -6,9 +6,11 @@ import javax.persistence.*;
 
 @Entity
 @Table(name = "role")
+// Line that stops exception because of a Hibernate~Jackson compatibilty issue, which ignores the handler
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class Role {
 
+    // Defining the id of the role so that it can be easily selected/joined with all of it's other relations
     @Id
     @Column(name = "rid")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -16,14 +18,23 @@ public class Role {
 
     String name;
 
-
+    // Defining super-constructor for the entity
     Role() {}
 
+    // Defining the object constructor
     public Role(String name) {
         this.name = name;
     }
 
+    /*
+        Usually, I would use Lombok for the Getters and 
+        Getters, but some IDE's don't support the plugin
+        and will show you errors when referencing the methods
+        even though they are there. I have used traditional Java
+        Setter and Getters for this project.
+    */
 
+    // Setter and getter for the role id
     public String getName() {
         return name;
     }
